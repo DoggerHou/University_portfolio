dbd93a92ec9e948efea0ac4b1e14f160dc5097da
DoggerHou <mr.slipa@mail.ru>
2025-11-15 03:18:14 +0500

    add files for OOP lab6

---
+++ OOP/lab6/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Laba_6
+{
+    public partial class Form1 : Form
+    {
+        Storage myStorage = new Storage();
+        bool controlUp = false;
+        Color btn_color = Color.Black;
+        bool ellipse = true;
+        bool square = false;
+        bool triangle = false;
+        PictureBox pBox;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)//отрисовка объектов
+        {
+            for (int i = 0; i < myStorage.getSize(); i++)
+            {
+                myStorage.getObject(i).OnPaint(e);
+            }
+
+        }
+
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.ActiveControl = null;
+            if (ellipse)//Добавление эллипса
+                myStorage.AddObject(new CCircle(e.Location, btn_color, pBox), e, controlUp);
+
+            if (square)//добавление квадрата
+                myStorage.AddObject(new CSquare(e.Location, btn_color, pBox), e, controlUp);
+            pictureBox.Invalidate();
+
+            if (triangle)//добавление треугольника
+                myStorage.AddObject(new Triangle(e.Location, btn_color, pBox), e, controlUp);
+            pictureBox.Invalidate();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Add) //обрабатывает нажатие: Увеличение объектов
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail() == true)
+                        myStorage.getObject(i).increase_Size();
+                }
+
+            if (e.KeyData == Keys.Subtract)//обрабатывает нажатие: Уменьшение объектов
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail() == true)
+                        myStorage.getObject(i).decrease_Size();
+                }
+
+            if (e.Control) //обрабатывает нажатие: Выбор объектов
+                controlUp = true;
+
+            if (e.KeyData == Keys.Delete)//обрабатывает нажатие: Удаление объектов
+                myStorage.delete_DetailedObjects();
+
+
+            //Далее идет обработка 4 кнопок движения, ничего интересного и сложного
+
+            if (e.KeyData == Keys.Right)//Движение вправо
+            {
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail())
+                        myStorage.getObject(i).move_Object(1, 0);
+                }
+            }
+            if (e.KeyData == Keys.Left)//Движение влево
+            {
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail())
+                        myStorage.getObject(i).move_Object(-1, 0);
+                }
+            }
+            if (e.KeyData == Keys.Down)//тут +1, ибо ось Y направлена вниз(а не как в православии, вверх)
+            {
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail())
+                        myStorage.getObject(i).move_Object(0, 1);
+                }
+            }
+            if (e.KeyData == Keys.Up)//тут -1, ибо ось Y направлена вниз(а не как в православии, вверх)
+            {
+                for (int i = 0; i < myStorage.getSize(); i++)
+                {
+                    if (myStorage.getObject(i).getDetail())
+                        myStorage.getObject(i).move_Object(0, -1);
+                }
+            }
+            pictureBox.Invalidate();
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)//Если кнопка кливиатуры отпущена, значит 
+        {                                                      //отпущен и ctrl
+            controlUp = false;
+        }
+
+
+        private void button_RED_Click(object sender, EventArgs e)   //Кнопки выбора цвета
+        {
+            btn_color = ((Button)sender).BackColor;
+            for (int i = 0; i < myStorage.getSize(); i++)
+            {
+                if (myStorage.getObject(i).getDetail())
+                    myStorage.getObject(i).object_color = btn_color;
+            }
+            pictureBox.Invalidate();
+            this.ActiveControl = null;
+        }
+
+
+        private void button_ELLIPSE_Click(object sender, EventArgs e)//Кнопки выбора объекта для отрисовки
+        {
+            if (sender == button_ELLIPSE)
+            {
+                ellipse = true;
+                square = false;
+                triangle = false;
+                button_ELLIPSE.BackColor = Color.Black;
+                button_SQUARE.BackColor = Color.DimGray;
+                button_TRIANGLE.BackColor = Color.DimGray;
+            }
+
+            else if (sender == button_SQUARE)
+            {
+                ellipse = false;
+                square = true;
+                triangle = false;
+                button_ELLIPSE.BackColor = Color.DimGray;
+                button_SQUARE.BackColor = Color.Black;
+                button_TRIANGLE.BackColor = Color.DimGray;
+            }
+            else if (sender == button_TRIANGLE)
+            {
+                ellipse = false;
+                square = false;
+                triangle = true;
+                button_ELLIPSE.BackColor = Color.DimGray;
+                button_SQUARE.BackColor = Color.DimGray;
+                button_TRIANGLE.BackColor = Color.Black;
+            }
+
+        }
+
+
+        private void button_ColorDialog_Click(object sender, EventArgs e)//кнопка для ColorDialog
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                btn_color = colorDialog1.Color;
+                ((Button)sender).BackColor = colorDialog1.Color;
+
+                for (int i = 0; i < myStorage.getSize(); i++)    //изменяем цвет у всех выбранных объектов
+                {
+                    if (myStorage.getObject(i).getDetail())
+                        myStorage.getObject(i).object_color = btn_color;
+                }
+                pictureBox.Invalidate();
+            }
+            this.ActiveControl = null;
+        }
+
+        private void pictureBox_Resize(object sender, EventArgs e)
+        {
+            pBox = pictureBox;
+            for(int i = myStorage.getSize() - 1; i >= 0; i--)
+            {
+                myStorage.getObject(i).pb = pBox;
+            }
+        }
+    }
+}
---
+++ OOP/lab6/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace OOP_Laba_6
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class Model
+    {
+        public Color object_color;//Цвет объекта
+        public PictureBox pb; //Для передачи размера формы для рисования
+        protected int RADIX; //Радиус окружности/Вписанной в квадрат окружности/Длина стороны треугольника
+        protected Point location; //Точка центра объекта
+        protected bool detail;    //Маркер выделенности
+
+
+
+        public Model(Point location, Color color, PictureBox pb)
+        {
+            RADIX = 40;
+            this.pb = pb;
+            this.location = location;
+            detail = true;
+            object_color = color;
+        }
+
+
+        public void changeDetail_toFalse() { detail = false; } //Снимает выделение c объекта
+
+
+        public bool getDetail() { return detail; } //Достает информацию о Выделенности объекта
+
+
+        public virtual void OnPaint(PaintEventArgs e) { }//Отрисовка
+
+
+        public virtual bool isPicked(MouseEventArgs e, bool controlUp) { return false; } //Попали ли мы в объект
+
+
+        public virtual void increase_Size() //Увеличить размер
+        {
+            if (check_Location(location.X, location.Y, RADIX))
+                RADIX += 2;
+        }
+
+
+        public virtual void decrease_Size() //Уменьшить размер
+        {
+            if (RADIX > 0)
+                RADIX -= 2;
+        }
+
+
+        public virtual void move_Object(int _X, int _Y) //Изменить местоположение объекта
+        {
+            if (check_Location(location.X + _X, location.Y + _Y, RADIX))
+            {
+                location.X += _X;
+                location.Y += _Y;
+            }
+        }
+
+
+        public bool check_Location(int point_X, int point_Y, int RADIX) //проверка на выход за границу для круга/квадрата
+        {
+            if ((point_X - RADIX >= 0) & (point_X + RADIX <= pb.Width) &
+                (point_Y - RADIX >= 0) & (point_Y + RADIX <= pb.Height))
+                return true;
+            return false;
+        }
+    }
+
+
+    public class CCircle : Model
+    {
+        //private int RADIX = 40; //Радиус круга
+
+
+        public CCircle(Point location, Color color, PictureBox pb) : base(location, color, pb) { }
+
+
+        public override void OnPaint(PaintEventArgs e)//Отрисовка Эллипса
+        {
+            Pen pen = new Pen(object_color);
+            if (detail)
+                pen.Width = 7;
+            else
+                pen.Width = 4;
+
+            e.Graphics.DrawEllipse(pen, location.X - RADIX, location.Y - RADIX, RADIX * 2, RADIX * 2);
+        }
+
+
+        public override bool isPicked(MouseEventArgs e, bool controlUp)//попали ли мы в объект
+        {
+            if (Math.Pow(location.X - e.X, 2) + Math.Pow(location.Y - e.Y, 2) <= Math.Pow(RADIX, 2)
+                & controlUp)
+            {
+                detail = !detail; //Инвертируем выделенность
+                return true;
+            }
+            return false;//не попал в круг - ниче не делает
+        }
+    }
+
+
+    public class CSquare : Model
+    {
+        //private int RADIX = 40;//Радиус вписанного в квадрат круга(или половина стороны квадрата, кому как нравится)
+
+
+        public CSquare(Point location, Color color, PictureBox pb) : base(location, color, pb) { }
+
+
+        public override void OnPaint(PaintEventArgs e)//Отрисовка Квадрата
+        {
+            Pen pen = new Pen(object_color);
+            if (detail)
+                pen.Width = 7;
+            else
+                pen.Width = 4;
+
+            e.Graphics.DrawRectangle(pen, location.X - RADIX, location.Y - RADIX, RADIX * 2, RADIX * 2);
+        }
+
+
+        public override bool isPicked(MouseEventArgs e, bool controlUp)//попали ли мы в объект
+        {
+            if ((e.X <= location.X + RADIX) & (e.X >= location.X - RADIX) &
+                (e.Y <= location.Y + RADIX) & (e.Y >= location.Y - RADIX) &
+                controlUp)
+            {
+                detail = !detail; //Инвертируем выделенность
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    public class Triangle : Model
+    {
+        //private int RADIX = 80; //Длина стороны треугольника
+        private Point A; //Координаты Трех точек треугольника
+        private Point B;
+        private Point C;
+
+
+        public Triangle(Point location, Color color, PictureBox pb) : base(location, color,pb)
+        {
+            RADIX *= 2;
+            refreshTriangle();
+        }
+
+
+        //Возвращает площадь треугольника
+        private double Triangle_Square(double aX, double aY, double bX, double bY, double cX, double cY)
+        {
+            return Math.Abs(bX * cY - cX * bY - aX * cY + cX * aY + aX * bY - bX * aY);
+        }
+
+
+        //Проверяет, вышел ли треугольник за границы
+        private bool check_Borders(int point_aX, int point_cY, int point_aY) //к примеру, point_aX - координата 
+        {                                                                    //точки А по оси Х
+            if ((point_aX >= 0) & (point_aX + RADIX <= pb.Width) &
+                (point_cY >= 0) & (point_aY <= pb.Height))
+                return true;
+            return false;
+        }
+
+        //обновляем координаты точек треугольника после каких-то изменений длины стороны
+        private void refreshTriangle() 
+        {
+            A.X = location.X - RADIX / 2;
+            A.Y = (int)(location.Y + Math.Sqrt(3) * RADIX / 6);
+
+            B.X = location.X + RADIX / 2;
+            B.Y = (int)(location.Y + Math.Sqrt(3) * RADIX / 6);
+
+            C.X = location.X;
+            C.Y = (int)(location.Y - Math.Sqrt(3) * RADIX / 3);
+        }
+
+
+        public override void OnPaint(PaintEventArgs e)//состоит из отрисовки трех линий
+        {
+            Pen pen = new Pen(object_color);
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round; //Чтобы линии были более закругленными
+
+            if (detail)
+                pen.Width = 7;
+            else
+                pen.Width = 4;
+
+            e.Graphics.DrawLine(pen, A.X, A.Y, B.X, B.Y);
+            e.Graphics.DrawLine(pen, A.X, A.Y, C.X, C.Y);
+            e.Graphics.DrawLine(pen, B.X, B.Y, C.X, C.Y);
+
+        }
+
+
+        public override bool isPicked(MouseEventArgs e, bool controlUp)//по методу сравнения площадей
+        {
+            if ((Triangle_Square(A.X, A.Y, B.X, B.Y, e.X, e.Y) + Triangle_Square(A.X, A.Y, e.X, e.Y, C.X, C.Y) +
+                Triangle_Square(e.X, e.Y, B.X, B.Y, C.X, C.Y) - Triangle_Square(A.X, A.Y, B.X, B.Y, C.X, C.Y) <= 0.01) &
+                controlUp)
+            {
+                detail = !detail; //Инвертируем выделенность
+                return true;
+            }
+            return false;
+        }
+
+
+        public override void increase_Size()
+        {
+            if (check_Borders(A.X, C.Y, A.Y))
+            {
+                RADIX += 4;
+                refreshTriangle();
+            }
+        }
+
+
+        public override void decrease_Size()
+        {
+            if (RADIX > 0)
+            {
+                RADIX -= 4;
+                refreshTriangle();
+            }
+        }
+
+
+        public override void move_Object(int _X, int _Y)
+        {
+            if (check_Borders(A.X + _X, C.Y + _Y, A.Y + _Y))
+            {
+                location.X += _X;
+                location.Y += _Y;
+                refreshTriangle();
+            }
+        }
+
+    }
+
+
+    public class Storage
+    {
+        private List<Model> objects;
+
+
+
+        public Storage()
+        {
+            objects = new List<Model>();
+        }
+
+
+        public int getSize() { return objects.Count(); } //возвращает размер Списка
+
+        public Model getObject(int index) { return objects[index]; } //возвращает объект Model
+
+
+        public void AddObject(Model temp_object, MouseEventArgs e, bool controlUp)  //добавляет объект
+        {
+            for (int i = 0; i < objects.Count(); i++) //Если попали в объект, то выходим
+                if (objects[i].isPicked(e, controlUp))      //(чтобы не рисовать новый)
+                {
+                    return;
+                }
+            objects.Add(temp_object); //если не попали, добавляем новый объект
+            for (int i = 0; i < objects.Count() - 1; i++)//снимаем выделение со всех предыдущих
+                objects[i].changeDetail_toFalse();
+        }
+
+
+        public void delete_DetailedObjects()    //удаляет все "помеченные" объекты
+        {
+            for (int i = objects.Count() - 1; i >= 0; i--)
+                if (objects[i].getDetail() == true)
+                    objects.RemoveAt(i);
+        }
+    }
+}
