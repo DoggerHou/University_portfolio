2067931f79d23dc3678811539916be070a1d549f
DoggerHou <mr.slipa@mail.ru>
2025-11-15 22:39:51 +0500

    add lab1

---
+++ Computer_networks/lab1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Laba_1__IP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.textBoxStartIP = new System.Windows.Forms.TextBox();
+            this.textBoxEndIP = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxMask = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.textBoxBroadcast = new System.Windows.Forms.TextBox();
+            this.textBoxMacAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Ip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DNS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxStartIP
+            // 
+            this.textBoxStartIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxStartIP.Location = new System.Drawing.Point(39, 444);
+            this.textBoxStartIP.Name = "textBoxStartIP";
+            this.textBoxStartIP.Size = new System.Drawing.Size(193, 26);
+            this.textBoxStartIP.TabIndex = 1;
+            // 
+            // textBoxEndIP
+            // 
+            this.textBoxEndIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxEndIP.Location = new System.Drawing.Point(39, 483);
+            this.textBoxEndIP.Name = "textBoxEndIP";
+            this.textBoxEndIP.Size = new System.Drawing.Size(193, 26);
+            this.textBoxEndIP.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(61, 511);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(139, 58);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Проверить";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxMask
+            // 
+            this.textBoxMask.Location = new System.Drawing.Point(866, 390);
+            this.textBoxMask.Name = "textBoxMask";
+            this.textBoxMask.Size = new System.Drawing.Size(201, 22);
+            this.textBoxMask.TabIndex = 5;
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Location = new System.Drawing.Point(866, 490);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(201, 22);
+            this.textBoxAdress.TabIndex = 6;
+            // 
+            // textBoxBroadcast
+            // 
+            this.textBoxBroadcast.Location = new System.Drawing.Point(866, 440);
+            this.textBoxBroadcast.Name = "textBoxBroadcast";
+            this.textBoxBroadcast.Size = new System.Drawing.Size(201, 22);
+            this.textBoxBroadcast.TabIndex = 7;
+            // 
+            // textBoxMacAdress
+            // 
+            this.textBoxMacAdress.Location = new System.Drawing.Point(866, 540);
+            this.textBoxMacAdress.Name = "textBoxMacAdress";
+            this.textBoxMacAdress.Size = new System.Drawing.Size(201, 22);
+            this.textBoxMacAdress.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(914, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Маска подсети";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(869, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Широковещательный адрес";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(927, 470);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Адрес сети";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(927, 520);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "MAC адрес";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ip,
+            this.DNS,
+            this.Status});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1011, 300);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Ip
+            // 
+            this.Ip.HeaderText = "IP adress";
+            this.Ip.MinimumWidth = 6;
+            this.Ip.Name = "Ip";
+            this.Ip.Width = 320;
+            // 
+            // DNS
+            // 
+            this.DNS.HeaderText = "DNS";
+            this.DNS.MinimumWidth = 6;
+            this.DNS.Name = "DNS";
+            this.DNS.Width = 320;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 320;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 572);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMacAdress);
+            this.Controls.Add(this.textBoxBroadcast);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.textBoxMask);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxEndIP);
+            this.Controls.Add(this.textBoxStartIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxStartIP;
+        private System.Windows.Forms.TextBox textBoxEndIP;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxMask;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.TextBox textBoxBroadcast;
+        private System.Windows.Forms.TextBox textBoxMacAdress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DNS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ Computer_networks/lab1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace Laba_1__IP
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            byte[] start_adress_bytes = IPAddress.Parse(textBoxStartIP.Text).GetAddressBytes();
+            byte[] end_adress_bytes = IPAddress.Parse(textBoxEndIP.Text).GetAddressBytes();
+            byte[] mask = new byte[4];
+            byte[] broadcast = new byte[4];
+            byte[] adressSeti = new byte[4];
+
+
+            bool edge = false;
+            for (int i = 0; i < 4; i++)//Рассчет маски
+            {
+                for (byte b = 128; b >= 1; b /= 2)
+                {
+                    if (!edge && (start_adress_bytes[i] & b) == (end_adress_bytes[i] & b))
+                        mask[i] |= b;
+                    else
+                    {
+                        edge = true;
+                        mask[i] = (byte)(mask[i] & ~b);
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < 4; i++)//считаем адрес сети и Широковещательный
+            {
+                adressSeti[i] = (byte)(start_adress_bytes[i] & mask[i]);
+                broadcast[i] = (byte)(start_adress_bytes[i] |~ mask[i]);
+            }
+
+            //Выводим данные в 4 текстБокса
+            textBoxMask.Text = mask[0].ToString() + '.' +
+                        mask[1].ToString() + '.' + mask[2].ToString()
+                        + '.' + mask[3].ToString();
+
+            textBoxAdress.Text = adressSeti[0].ToString() + '.' +
+                        adressSeti[1].ToString() + '.' + adressSeti[2].ToString()
+                        + '.' + adressSeti[3].ToString();
+
+            textBoxBroadcast.Text = broadcast[0].ToString() + '.' +
+                        broadcast[1].ToString() + '.' + broadcast[2].ToString()
+                        + '.' + broadcast[3].ToString();
+
+            textBoxMacAdress.Text = NetworkInterface.GetAllNetworkInterfaces()
+                        .Where(nic => nic.OperationalStatus == OperationalStatus.Up &&
+                        nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                        .Select(nic => nic.GetPhysicalAddress().ToString())
+                        .FirstOrDefault();
+
+
+            int rows_index = 0;
+            while (start_adress_bytes[0] != end_adress_bytes[0] ||
+                start_adress_bytes[1] != end_adress_bytes[1] ||
+                start_adress_bytes[2] != end_adress_bytes[2] ||
+                start_adress_bytes[3] != end_adress_bytes[3])
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[rows_index].Cells[0].Value =
+                start_adress_bytes[0].ToString() + '.' +
+                start_adress_bytes[1].ToString() + '.' +
+                start_adress_bytes[2].ToString() + '.' +
+                start_adress_bytes[3].ToString();
+
+                IPHostEntry host1;
+                IPAddress DNS = new IPAddress(start_adress_bytes);
+                try
+                {
+                    host1 = Dns.GetHostEntry(DNS);
+                    dataGridView1.Rows[rows_index].Cells[1].Value = host1.HostName;
+                    dataGridView1.Rows[rows_index].Cells[2].Value = "OK";
+                }
+                catch (Exception ex)
+                {
+                    dataGridView1.Rows[rows_index].Cells[1].Value = "------";
+                    dataGridView1.Rows[rows_index].Cells[2].Value = "Not OK";
+                }
+                rows_index += 1;
+
+                if (start_adress_bytes[3] + 1 > 255)
+                {
+                    if (start_adress_bytes[2] + 1 > 255)
+                    {
+                        if (start_adress_bytes[1] + 1 > 255)
+                        {
+                            start_adress_bytes[0] += 1;
+                            start_adress_bytes[1] = 0;
+                            start_adress_bytes[2] = 0;
+                            start_adress_bytes[3] = 0;
+                        }
+                        start_adress_bytes[1] += 1;
+                        start_adress_bytes[2] = 0;
+                        start_adress_bytes[3] = 0;
+                    }
+                    start_adress_bytes[2] += 1;
+                    start_adress_bytes[3] = 0;
+                }
+                start_adress_bytes[3] += 1;
+            }
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[rows_index].Cells[0].Value =
+                start_adress_bytes[0].ToString() + '.' +
+                start_adress_bytes[1].ToString() + '.' +
+                start_adress_bytes[2].ToString() + '.' +
+                start_adress_bytes[3].ToString();
+
+            IPHostEntry host2;
+            IPAddress DNS2 = new IPAddress(start_adress_bytes);
+            try
+            {
+                host2 = Dns.GetHostEntry(DNS2);
+                dataGridView1.Rows[rows_index].Cells[1].Value = host2.HostName;
+                dataGridView1.Rows[rows_index].Cells[2].Value = "OK";
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows[rows_index].Cells[1].Value = "------";
+                dataGridView1.Rows[rows_index].Cells[2].Value = "Not OK";
+
+            }
+        }
+    }
+}
+
